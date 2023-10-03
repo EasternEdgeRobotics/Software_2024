7defef7c15db5adbd0a9118531e4cfdfcc36c123
Evan <41494790+evaan@users.noreply.github.com>
2023-10-03 01:37:06 -0230

    Added demo camera preview

--- GUI/src/App.jsx
+++ GUI/src/App.jsx
@@ -1,6 +1,6 @@
-import { CssBaseline, ThemeProvider, createTheme, Tabs, Tab } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme, Tabs, Tab, Grid, Box } from "@mui/material";
 import SafetyDisclaimer from "./SafetyDisclaimer";
-import React from "react";
+import React, { useEffect } from "react";
 import { CameraAlt, SportsEsports, Build } from "@mui/icons-material";
 
 const darkTheme = createTheme({
@@ -20,8 +20,19 @@ function App() {
           <Tab label=<SportsEsports /> />
           <Tab label=<Build /> />
       </Tabs>
+      <br/>
       {tabIndex === 0 && (
-        <h1>CAMERA WINDOW</h1>
+        <Box>
+          <Grid container spacing={2} style={{transform: `translateX(${(window.innerWidth - 1776)/2}px)`}}>
+            <Grid item xs margin="0">
+              <Box bgcolor="#ff0000" width="1280px" height="720px"><h1>Camera #1</h1></Box>
+            </Grid>
+            <Grid item xs margin="0">
+              <Box bgcolor="#00ff00" width="480px" height="352px"><h1>Camera #2</h1></Box>
+              <Box bgcolor="#0000ff" width="480px" height="352px" marginTop={"-6px"}><h1>Camera #3</h1></Box>
+            </Grid>
+          </Grid>
+        </Box>
       )}
       {tabIndex === 1 && (
         <h1>CONTROLLER WINDOW</h1>
