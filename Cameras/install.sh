4c6dac8c202dbdd0b9fc6846ed4bd1ee0d7093ef
Evan <41494790+evaan@users.noreply.github.com>
2023-10-10 21:45:08 -0230

    Fix install script

--- Cameras/install.sh
+++ Cameras/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo raspi-config nonint do_legacy 0
+sudo apt update
 sudo apt install -y cmake libjpeg-dev gcc g++
 sudo cp eer-camera.service /etc/systemd/system/eer-camera.service
 cd mjpg-streamer/mjpg-streamer-experimental
