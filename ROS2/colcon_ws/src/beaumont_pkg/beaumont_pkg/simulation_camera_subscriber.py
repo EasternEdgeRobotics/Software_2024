6aa7784ea7173af32a97fcc7839af38abaf3857e
Zaid-Duraid <zaidahmedduraid@gmail.com>
2024-02-11 15:11:55 -0330

    feat: Simulation camera recieved in GUI + angular movement working

--- ROS2/colcon_ws/src/beaumont_pkg/beaumont_pkg/simulation_camera_subscriber.py
+++ ROS2/colcon_ws/src/beaumont_pkg/beaumont_pkg/simulation_camera_subscriber.py
@@ -1,40 +1,87 @@
 import rclpy
 from rclpy.node import Node 
-from eer_messages.msg import ThrusterMultipliers
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from cv_bridge import CVBridge, CVBridgeError
+from cv_bridge import CvBridge, CvBridgeError
 import cv2 
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
 
+camera_0_capture = None
+camera_1_capture = None
 
-class ThrusterDataSubscriber(Node):
+class SimulationCameraSubscriber(Node):
+	
+	def __init__(self):
+		super().__init__('simulation_camera_subscriber')
+		self.simulation_camera_listener = self.create_subscription(Image, '/demo_cam/camera0/image_raw', self.simulation_camera_listener_callback, 10)
+		self.bridge = CvBridge()
+		
+	def simulation_camera_listener_callback(self, msg):
+		
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+		except CvBridgeError as e:
+			print(e)
+		
+		global camera_0_capture
+		camera_0_capture = cv_image
 
-    def __init__(self):
-        super().__init__('simulation_camera_subscriber')
-        self.bridge = CVBridge()
-        self.image_sub = 
+		
+        
 
-    def copilot_listener_callback(self, msg):
-        self.get_logger().info(f"{msg} recieved in ThrusterControl")
-        #TODO ROV Math
+class CamHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		if self.path.endswith('.mjpg'):
+			self.send_response(200)
+			self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
+			self.end_headers()
+			while True:
+				try:
+					imgRGB=cv2.cvtColor(camera_0_capture,cv2.COLOR_BGR2RGB)
+					ret, jpg = cv2.imencode('.jpg', imgRGB)
+					self.wfile.write("--jpgboundary".encode("utf-8"))
+					self.send_header('Content-type','image/jpeg')
+					self.send_header('Content-length',str(jpg.size))
+					self.end_headers()
+					self.wfile.write(jpg.tostring())
+					#time.sleep(0.05)
+				except KeyboardInterrupt:
+					break
+			return
+		if self.path.endswith('.html'):
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write('<html><head></head><body>')
+			self.wfile.write('<img src="http://192.168.2.199:8888/cam.mjpg"/>')
+			self.wfile.write('</body></html>')
+			return
 
-    def pilot_listener_callback(self, msg):  
-        controller_input = json.loads(msg.data)
-        thruster_directions = ("surge", "sway", "heave", "pitch", "roll", "yaw")
-        if (controller_input[1] in thruster_directions):
-            print(controller_input)
-            #TODO ROV Math
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+	"""Handle requests in a separate thread."""
 
+def runserver():
+	try:
+		server = ThreadedHTTPServer(('192.168.2.199', 8888), CamHandler)
+		print("Server otg")
+		server.serve_forever()
+	except KeyboardInterrupt:
+		server.socket.close()
 
 def main(args=None):
-    rclpy.init(args=args)
+	rclpy.init(args=args)
 
-    thruster_data_subscriber = ThrusterDataSubscriber()
+	simulation_data_subscriber = SimulationCameraSubscriber()
 
-    rclpy.spin(thruster_data_subscriber)
+	server = threading.Thread(target=runserver, daemon=True)
+	server.start()
 
-    thruster_data_subscriber.destroy_node()
-    rclpy.shutdown()
+	rclpy.spin(simulation_data_subscriber)
+	
+		
+	simulation_data_subscriber.destroy_node()
+	rclpy.shutdown()
 
 
 if __name__ == "__main__":
--- ROS2/colcon_ws/src/beaumont_pkg/beaumont_pkg/simulation_thruster_control.py
+++ ROS2/colcon_ws/src/beaumont_pkg/beaumont_pkg/simulation_thruster_control.py
@@ -14,10 +14,11 @@ class ThrusterDataSubscriber(Node):
         self.copilot_listener = self.create_subscription(ThrusterMultipliers, 'thruster_multipliers', self.copilot_listener_callback, 10)
         self.pilot_listener = self.create_subscription(String, 'controller_input', self.pilot_listener_callback, 10)
         self.simulation_velocity_publisher = self.create_publisher(Wrench, "/demo/link/simulation_model_linear_velocity", 10) 
+        self.simulation_angular_velocity_publishers = {"pitch":self.create_publisher(Wrench, "/demo/link/force_demo_pitch", 10),
+                                                    "roll":self.create_publisher(Wrench, "/demo/link/force_demo_roll", 10),
+                                                    "yaw":self.create_publisher(Wrench, "/demo/link/force_demo_yaw", 10)}
 
         self.lock = threading.Lock() #Used as a synchronization tool
-
-        #From the simulation, it appears that a force of 750N (not really a force) is just equivalent to giving it a velocity of ~0.2m/s. Tune this number later.
 
         self.force_input_array = {"surge":0,
                                     "sway":0,
@@ -26,10 +27,17 @@ class ThrusterDataSubscriber(Node):
         self.velocity_array = {"surge":0,
                                     "sway":0,
                                     "heave":0}
+        
+        self.angular_velocity_array = {"pitch":0,
+                                       "roll":0,
+                                       "yaw":0}
 
         self.force_input_time_array = {"surge":0,
                                                 "sway":0,
-                                                "heave":0}
+                                                "heave":0,
+                                                "pitch":0,
+                                                "roll":0,
+                                                "yaw":0}
 
         self.previous_force_input_array = {"surge":0,
                                     "sway":0,
@@ -42,17 +50,20 @@ class ThrusterDataSubscriber(Node):
         self.surface_area_for_drag = {"surge":0.1813,
                                         "sway":0.2035,
                                         "heave":0.2695}
-
-        self.thread_lock = {"surge":True,
-                            "sway":True,
-                            "heave":True}
+        
+        self.thruster_force_multipliers = {"power":0,
+                                        "surge":0,
+                                        "sway":0,
+                                        "heave":0,
+                                        "pitch":0,
+                                        "roll":0,
+                                        "yaw":0}
 
         self.bot_mass = 23 #23kg
         self.fluid_mass_density = 1000 #1000kg/m^3
         self.drag_coefficient = 0.025 #This value has been tuned
-        self.force_adjustment_factor = 0.075 #It seems to me like the momentum calculations are giving something an order of magnitude higher than what they should. 
-                                            #Maybe because of the the python time module. This adjustment factor should be tuned to be more realistic 
         self.max_thruster_force = 116
+        self.gazebo_force_adjustment_factor = 150
         self.surge_velocity_thread = threading.Thread(target=self.calculate_velocity,args=("surge",) , daemon=True)
         self.sway_velocity_thread = threading.Thread(target=self.calculate_velocity,args=("sway",) , daemon=True)
         self.heave_velocity_thread = threading.Thread(target=self.calculate_velocity,args=("heave",) , daemon=True)
@@ -61,22 +72,79 @@ class ThrusterDataSubscriber(Node):
         self.sway_velocity_thread.start()
         self.heave_velocity_thread.start()
 
+        
+        #Should be 4775 to match real velocity... but this is too fast 
+        #I persume the the parameters in the calculations are off or that the Python time modules is off
+        #Assuming the terminal velocity of the bot in water is 0.25 m/s... the force adjustment factor should be 150
+
         # prevent unused variable warning
         self.copilot_listener 
         self.pilot_listener
 
     def copilot_listener_callback(self, msg):
         self.get_logger().info(f"{msg} recieved in ThrusterControl")
-        #TODO ROV Math
+        self.lock.acquire()
+        self.thruster_force_multipliers["power"] = msg.power/100
+        self.thruster_force_multipliers["surge"] = msg.surge/100
+        self.thruster_force_multipliers["sway"] = msg.sway/100
+        self.thruster_force_multipliers["heave"] = msg.heave/100
+        self.thruster_force_multipliers["pitch"] = msg.pitch/100
+        self.thruster_force_multipliers["roll"] = msg.roll/100
+        self.thruster_force_multipliers["yaw"] = msg.yaw/100
+        self.lock.release()
 
     def pilot_listener_callback(self, msg):  
         controller_input = json.loads(msg.data)
+
+        if (controller_input[1].split("-") == 2): #We may get inputs such as "heave-up" or "pitch-down", where the button is being used as an axis
+            input = 1 if controller_input[1].split("-")[1] == "up" else -1 
+            controller_input[1] = controller_input[1].split("-")[0]
+            controller_input[0] = input        
+
         accepted_thruster_directions = ("surge", "sway", "heave", "pitch", "roll", "yaw") #This script is only responsible for thruster control related actions
         if (controller_input[1] in accepted_thruster_directions[0:3]):
             self.lock.acquire()
             self.force_input_array[controller_input[1]] = float(controller_input[0])*self.max_thruster_force
-            #print(self.force_input_array)
             self.lock.release()
+        elif (controller_input[1] in accepted_thruster_directions[3:]):
+            #Will not do drag calculations for angular velocity
+            self.lock.acquire()
+
+            self.angular_velocity_array[controller_input[1]] = float(controller_input[0] * self.gazebo_force_adjustment_factor * self.thruster_force_multipliers["power"]*self.thruster_force_multipliers[controller_input[1]])
+
+            pitch_velocity = Wrench()
+            roll_velocity = Wrench()
+            yaw_velocity = Wrench()
+
+            pitch_velocity.force.z = float(self.angular_velocity_array["pitch"])
+            roll_velocity.force.z = float(self.angular_velocity_array["roll"])
+            yaw_velocity.force.x = float(self.angular_velocity_array["yaw"] * -1)
+
+            self.simulation_angular_velocity_publishers["pitch"].publish(pitch_velocity)
+            self.simulation_angular_velocity_publishers["roll"].publish(roll_velocity)
+            self.simulation_angular_velocity_publishers["yaw"].publish(yaw_velocity)
+
+            self.reset_unused_angular_velocity()
+            self.lock.release()
+
+    def reset_unused_angular_velocity(self):
+        #Reset angular movement after 0.5 seconds of inactivity
+        pitch_velocity = Wrench()
+        roll_velocity = Wrench()
+        yaw_velocity = Wrench()
+
+        if (time() - self.force_input_time_array["pitch"] > 10):
+            self.simulation_angular_velocity_publishers["pitch"].publish(pitch_velocity)
+            self.force_input_time_array["pitch"] = time()
+            
+        if (time() - self.force_input_time_array["roll"] > 10):
+            self.simulation_angular_velocity_publishers["roll"].publish(roll_velocity)
+            self.force_input_time_array["roll"] = time()
+
+
+        if (time() - self.force_input_time_array["yaw"] > 10):
+            self.simulation_angular_velocity_publishers["yaw"].publish(yaw_velocity)
+            self.force_input_time_array["yaw"] = time()
 
     def calculate_velocity(self, direction):
         i = 0
@@ -84,9 +152,8 @@ class ThrusterDataSubscriber(Node):
             self.lock.acquire()
             if self.force_input_time_array[direction] == 0:
                 self.force_input_time_array[direction] = time()
-                #print("A time reading: ", self.force_input_time_array[direction])
             else:
-                drag_force = 0.5*self.fluid_mass_density*((self.velocity_array[direction]/self.force_adjustment_factor)**2)*self.drag_coefficient*self.surface_area_for_drag[direction]
+                drag_force = 0.5*self.fluid_mass_density*((self.velocity_array[direction])**2)*self.drag_coefficient*self.surface_area_for_drag[direction]
 
                 thruster_force = self.force_input_array[direction]
 
@@ -100,11 +167,11 @@ class ThrusterDataSubscriber(Node):
                     self.previous_force_input_array[direction] = 0
 
                 if (self.velocity_array[direction]>0):
-                    drag_force=drag_force*(-1) #Drag force acts such that it opposes motion. Technically it should oppose velocity and not force... but it's too jank
+                    drag_force=drag_force*(-1) #Drag force acts such that it opposes motion.
 
                 net_force = thruster_force + drag_force
 
-                self.velocity_array[direction] = self.velocity_array[direction]+(((time()-self.force_input_time_array[direction])*net_force*self.force_adjustment_factor)/self.bot_mass) #Momentum divided by mass
+                self.velocity_array[direction] = self.velocity_array[direction]+(((time()-self.force_input_time_array[direction])*net_force)/self.bot_mass) #Momentum divided by mass
 
                 if (time()-self.force_input_time_array[direction] >= 1): #No input for 1 seconds
                     self.force_input_array[direction] = 0
@@ -113,13 +180,14 @@ class ThrusterDataSubscriber(Node):
 
                 velocity = Wrench()
 
-                velocity.force.x = float((-1)*self.velocity_array["sway"] * 750*2) #Gazebo is weird, 750*2 is an adjustment factor to give actual velocity 
+                velocity.force.x = float((-1)*self.velocity_array["sway"] * self.gazebo_force_adjustment_factor * self.thruster_force_multipliers["power"] * self.thruster_force_multipliers["sway"])  
 
-                velocity.force.y = float(self.velocity_array["surge"] * 750*2)
+                velocity.force.y = float(self.velocity_array["surge"] * self.gazebo_force_adjustment_factor * self.thruster_force_multipliers["power"] * self.thruster_force_multipliers["surge"])
 
-                velocity.force.z = float((-1)*self.velocity_array["heave"] * 750*2)
+                velocity.force.z = float((-1)*self.velocity_array["heave"] * self.gazebo_force_adjustment_factor * self.thruster_force_multipliers["power"] * self.thruster_force_multipliers["heave"])
 
                 self.simulation_velocity_publisher.publish(velocity)
+                self.reset_unused_angular_velocity()
                 
             i = i + 1
             self.lock.release()
