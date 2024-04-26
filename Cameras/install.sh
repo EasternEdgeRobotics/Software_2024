b069da2a2c4894ab0b8762d33ac428110dc5319a
Zaid-Duraid <zaduraid@mun.ca>
2024-04-26 17:24:41 -0230

    feat: all_in_one_pi4_installer.sh for easy environment setup

--- Cameras/install.sh
+++ Cameras/install.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
-sudo raspi-config nonint do_legacy 0
-sudo apt update
-sudo apt install -y cmake libjpeg-dev gcc g++
-sudo cp eer-camera.service /etc/systemd/system/eer-camera.service
-cd mjpg-streamer/mjpg-streamer-experimental
-make
-sudo make install
-sudo systemctl enable --now eer-camera
-echo "########################"
-echo " Installation complete!"
-echo "########################"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y python3-picamera2 python3-libcamera git make
+git clone https://github.com/roamingthings/spyglass
+cd spyglass
+mkdir -p ~/printer_data/config
+make install
+cd ..
+sudo cp spyglass.conf ~/printer_data/config/spyglass.conf
+sudo cp spyglass.service /etc/systemd/system/spyglass.service
+sudo systemd daemon-reload
+sudo systemctl enable --now spyglass
+echo "all done, make sure to reboot"
---
+++ all_in_one_pi4_installer.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+cd ~/Software_2024
+
+# Convert docker_entrypoint.sh (for backend) to unix to fix docker erroring out because of windows (dos-style) line endings
+sudo apt-get install dos2unix -y
+sudo dos2unix ROS2/colcon_ws/docker_entrypoint.sh
+sudo chmod +x ROS2/colcon_ws/docker_entrypoint.sh
+
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# Start up and frontend and backend containers as a daemon (background) recurring process
+sudo docker compose up -d 
+
+# Install and setup the camera streamer
+cd Cameras
+. install.sh
