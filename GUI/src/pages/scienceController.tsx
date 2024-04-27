d33252e1a621c1c2d25b5f2bfa8646e5fa1e40ff
Inusha <inusha12345@gmail.com>
2024-04-27 17:38:34 -0230

    comments and formatting

--- GUI/src/pages/scienceController.tsx
+++ GUI/src/pages/scienceController.tsx
@@ -36,39 +36,54 @@ import ROSLIB, { Ros } from "roslib";
 import React from "react";
 import { ROSIP } from "../api/Atoms";
 
-
-const redirectToScreenshot = async (urls: string[], i:0|1|2|3) => {
+const redirectToScreenshot = async (urls: string[], i: 0 | 1 | 2 | 3) => {
   //open a new tab with the stream url
-
   let streamUrl = urls[i];
-  streamUrl = streamUrl.replace("/stream", "/snapshot")
+  streamUrl = streamUrl.replace("/stream", "/snapshot");
   window.open(streamUrl, "_blank");
 };
 
 const ScreenshotVeiw = ({ urls }: { urls: string[] }) => {
+  //[To-do] better styling for the components
   console.log(urls, "URLS");
   return (
     <div className="desktop1-rectangle1">
       <div className="desktop1-frame1">
-        <button type="button" className="desktop1-button button" onClick={() => redirectToScreenshot(urls, 0)}>
+        <button
+          type="button"
+          className="desktop1-button button"
+          onClick={() => redirectToScreenshot(urls, 0)}
+        >
           <span className="desktop1-text">
             <span>Camera 1</span>
             <br></br>
           </span>
         </button>
-        <button type="button" className="desktop1-button1 button" onClick={() => redirectToScreenshot(urls, 1)}>
+        <button
+          type="button"
+          className="desktop1-button1 button"
+          onClick={() => redirectToScreenshot(urls, 1)}
+        >
           <span className="desktop1-text03">
             <span>Camera 2</span>
             <br></br>
           </span>
         </button>
-        <button type="button" className="desktop1-button2 button" onClick={() => redirectToScreenshot(urls, 2)}>
+        <button
+          type="button"
+          className="desktop1-button2 button"
+          onClick={() => redirectToScreenshot(urls, 2)}
+        >
           <span className="desktop1-text06">
             <span>Camera 3</span>
             <br></br>
           </span>
         </button>
-        <button type="button" className="desktop1-button3 button" onClick={() => redirectToScreenshot(urls, 3)}>
+        <button
+          type="button"
+          className="desktop1-button3 button"
+          onClick={() => redirectToScreenshot(urls, 3)}
+        >
           <span className="desktop1-text09">
             <span>Camera 4</span>
             <br></br>
@@ -82,10 +97,13 @@ const ScreenshotVeiw = ({ urls }: { urls: string[] }) => {
   );
 };
 
+//recursive function to render the tasks
+//level is used to calculate the padding of the tasks
 function renderTasks(tasks: Task[], level = 0) {
-  // if (!(level >= 1 || tasks.length > 2)) level = level - 1;
 
   const handleCheckboxChange = (task: Task) => {
+    //[To-do] Update the tast status globally with ROS
+    //[To-do] Listen and update the task status from ROS
     task.checked = !task.checked;
     const updatedTask = { ...task, checked: !task.checked };
     localStorage.setItem(task.name, JSON.stringify(updatedTask));
@@ -160,7 +178,7 @@ function renderTasks(tasks: Task[], level = 0) {
   });
 }
 
-function SubList(props: { name: string; tasks: Task[]; max?:boolean}) {
+function SubList(props: { name: string; tasks: Task[]; max?: boolean }) {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [score, setScore] = useState<number>(0);
   const [totalScore, setTotalScore] = useState<number>(0);
@@ -174,15 +192,25 @@ function SubList(props: { name: string; tasks: Task[]; max?:boolean}) {
   }, [props.tasks]);
 
   useEffect(() => {
+    //recursive function to calculate the total score of the tasks
+    //if max is true, return the maximum score of the tasks [used for when only 1 task needs to be completed]
+    const calculateTotalScore = (tasks: Task[], max = false): number => {
+      if (max)
+        return Math.max(
+          ...tasks.map(
+            (t) => t.points ?? calculateTotalScore(t.subTasks!, t.single)
+          )
+        );
 
-    const calculateTotalScore = (tasks: Task[], max=false): number => {
-      if (max) return Math.max(...tasks.map(t =>t.points?? calculateTotalScore((t.subTasks)!, t.single)))
-      
       let total = 0;
       tasks.forEach((subtask) => {
-        if (subtask.single) console.log("subtask", subtask, subtask.points??0,subtask.single)
+        if (subtask.single)
+          console.log("subtask", subtask, subtask.points ?? 0, subtask.single);
         if (subtask.subTasks) {
-            total += calculateTotalScore(subtask.subTasks, subtask.single??false);
+          total += calculateTotalScore(
+            subtask.subTasks,
+            subtask.single ?? false
+          );
         } else {
           total += subtask.points ?? 0;
         }
@@ -191,7 +219,7 @@ function SubList(props: { name: string; tasks: Task[]; max?:boolean}) {
       return total;
     };
 
-    setTotalScore(calculateTotalScore(tasks,props.max));
+    setTotalScore(calculateTotalScore(tasks, props.max));
   }, [tasks]);
 
   return (
@@ -213,6 +241,7 @@ function SubList(props: { name: string; tasks: Task[]; max?:boolean}) {
 }
 
 function SideBar() {
+  //[To-do] acoount for mobile view
   const [collapsed, setCollapsed] = useState(false);
 
   const handleToggleSidebar = () => {
@@ -275,7 +304,11 @@ function SideBar() {
             name="TASK 3 : From the Red Sea to Tenesse"
             tasks={taskJSON.task3.tasks}
           />
-          <SubList name="TASK 4 : MATE Floats" tasks={taskJSON.task4.tasks} max={taskJSON.task4.single} />
+          <SubList
+            name="TASK 4 : MATE Floats"
+            tasks={taskJSON.task4.tasks}
+            max={taskJSON.task4.single}
+          />
           {/* More menu items... */}
         </Menu>
       </Sidebar>
@@ -283,7 +316,10 @@ function SideBar() {
   );
 }
 
+//
 function CSVHandler() {
+  //[To-do] handle a CSV fike with title Row
+  //[To-do] formatting and layout
   ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -321,25 +357,25 @@ function CSVHandler() {
 
     reader.onload = (e) => {
       if (e.target) {
+        // Parsing CSV file contents.
         const contents = e.target.result as string;
-        //const lines = contents.split("\n");
-        //const header = lines[0].split(",");
-
         const headers = contents.split("\n").map((line) => line.split(",")[0]);
-        const lines = contents.split("\n").map((line) => line.split(",").slice(1).map(Number));
+        const lines = contents
+          .split("\n")
+          .map((line) => line.split(",").slice(1).map(Number));
 
         console.log("Headers:", headers, "Lines:", lines);
 
         const datasets: ChartData["datasets"] = [];
         let labels: string[] = [];
 
-      
         const colors = [
           "rgb(0, 143, 136)",
           "rgb(245, 200, 0)",
           "rgb(241, 98, 8)",
         ];
 
+        // Looping through headers to create datasets
         for (let i = 1; i < headers.length; i++) {
           const key = headers[i]?.trim() ?? "key ??";
 
@@ -347,17 +383,17 @@ function CSVHandler() {
 
           datasets.push({
             label: key,
-            data: lines[i-1],
+            data: lines[i - 1],
             borderColor: `${color.slice(0, -1)}, 0.75)`,
             backgroundColor: `${color.slice(0, -1)}, 0.95)`,
           });
         }
         labels = lines[0].map((item) => item.toString());
-        
+
         console.log("Data sets:", datasets, "Labels:", labels);
 
         const data: ChartData = {
-          labels, 
+          labels,
           datasets,
         };
 
@@ -371,38 +407,38 @@ function CSVHandler() {
     }
   };
 
-return (
-  <div>
-    <input type="file" accept=".csv" onChange={handleFileChange} />
-    <div
-      style={{
-        backgroundColor: "rgba(225, 225, 225, 0.8)",
-        margin: "3vh",
-        padding: "10px",
-        borderRadius: "10px",
-        backdropFilter: "blur(10px)",
-        WebkitBackdropFilter: "blur(10px)",
-        border: "1px solid rgba(255, 255, 255, 0.18)",
-        // height: chartData.datasets.length > 0 ? undefined : "150px",
-      }}
-    >
-      <Line data={chartData as any} options={options} />
+  return (
+    <div>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+      <div
+        style={{
+          backgroundColor: "rgba(225, 225, 225, 0.8)",
+          margin: "3vh",
+          padding: "10px",
+          borderRadius: "10px",
+          backdropFilter: "blur(10px)",
+          WebkitBackdropFilter: "blur(10px)",
+          border: "1px solid rgba(255, 255, 255, 0.18)",
+          // height: chartData.datasets.length > 0 ? undefined : "150px",
+        }}
+      >
+        <Line data={chartData as any} options={options} />
+      </div>
+      <div
+        style={{
+          backgroundColor: "rgba(225, 225, 225, 0.8)",
+          margin: "3vh",
+          padding: "10px",
+          borderRadius: "10px",
+          backdropFilter: "blur(10px)",
+          WebkitBackdropFilter: "blur(10px)",
+          border: "1px solid rgba(255, 255, 255, 0.18)",
+        }}
+      >
+        <Bar data={chartData as any} options={options} />
+      </div>
     </div>
-    <div
-      style={{
-        backgroundColor: "rgba(225, 225, 225, 0.8)",
-        margin: "3vh",
-        padding: "10px",
-        borderRadius: "10px",
-        backdropFilter: "blur(10px)",
-        WebkitBackdropFilter: "blur(10px)",
-        border: "1px solid rgba(255, 255, 255, 0.18)",
-      }}
-    >
-      <Bar data={chartData as any} options={options} />
-    </div>
-  </div>
-);
+  );
 }
 
 export function ControllerApp() {
@@ -410,44 +446,46 @@ export function ControllerApp() {
   const [ros, setRos] = React.useState<Ros>(new ROSLIB.Ros({}));
   const [RosIP] = useAtom(ROSIP);
   const [urls, setURLs] = React.useState<string[]>([]);
-  
+
   React.useEffect(() => {
+    ros.connect(`ws://${RosIP}:9090`);
+    setInterval(() => {
+      if (!ros.isConnected) {
+        setRos(new ROSLIB.Ros({}));
         ros.connect(`ws://${RosIP}:9090`);
-        setInterval(() => {
-            if (!ros.isConnected) {
-                setRos(new ROSLIB.Ros({}));
-                ros.connect(`ws://${RosIP}:9090`);
-            }
-        }, 1000);
+      }
+    }, 1000);
   }, []);
-  
+
   // Create a ROS service on the "/camera_urls" topic, using a custom EER service type (see eer_messages folder in ROS2/colcon_ws/src)
-    const cameraURLsClient = new ROSLIB.Service({ros:ros,
-                                                name:"/camera_urls",
-      serviceType: "eer_messages/Config"
-    });
-  
-  if (urls.length == 0)
-  {
+  const cameraURLsClient = new ROSLIB.Service({
+    ros: ros,
+    name: "/camera_urls",
+    serviceType: "eer_messages/Config",
+  });
+
+  if (urls.length == 0) {
     const request = new ROSLIB.ServiceRequest({
-                state:1,
-                data:"FetchCameraURLs"}); // What's in the data field is not important in this case
-            cameraURLsClient.callService(request, function(result){
-              try {
-        if (result.result.trim() !="[]"){ // Trim spaces before comparison
+      state: 1,
+      data: "FetchCameraURLs",
+    }); // What's in the data field is not important in this case
+    cameraURLsClient.callService(request, function (result) {
+      try {
+        if (result.result.trim() != "[]") {
+          // Trim spaces before comparison
           camUrls = JSON.parse(result.result);
           setURLs(camUrls);
-         //   console.log("camera_urls", camUrls);
+          //   console.log("camera_urls", camUrls);
         } else {
-            console.log("No camera URLs stored in database");
+          console.log("No camera URLs stored in database");
         }
-    } catch (error) {
+      } catch (error) {
         console.error("Error parsing result:", error); // Log parsing errors
-    }
-            });
+      }
+    });
   }
-  
-console.log("URLS", urls)
+
+  console.log("URLS", urls);
 
   const styles = {
     contentDiv: {
