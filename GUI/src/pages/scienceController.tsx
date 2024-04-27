a21819a7f4db31a92032832d2a7ceb5e847fe6cf
Inusha <inusha12345@gmail.com>
2024-04-27 05:21:00 -0230

    line + bar chart

--- GUI/src/pages/scienceController.tsx
+++ GUI/src/pages/scienceController.tsx
@@ -9,18 +9,17 @@ import { CameraIPs } from "../api/Atoms";
 
 import { useEffect, useRef, useState } from "react";
 
-
 import "../styles/science.css";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Home } from "@mui/icons-material";
 import "../styles/science.css";
 import { Col, Row } from "react-bootstrap";
 import taskJSON from "./tasks.json";
-import {Task} from "../types/Task";
+import { Task } from "../types/Task";
 import { MenuSquareIcon } from "lucide-react";
 import SquareIcon from "@mui/icons-material/Square";
 
-import { Chart, Line,ChartProps } from 'react-chartjs-2';
+import { Chart, Line, ChartProps, Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -31,44 +30,43 @@ import {
   Tooltip,
   Legend,
   ChartData,
-  ChartType
-} from 'chart.js';
-
+  ChartType,
+  BarElement,
+} from "chart.js";
 
 const ScreenshotVeiw = () => {
   return (
-        <div className="desktop1-rectangle1">
-          <div className="desktop1-frame1">
-            <button type="button" className="desktop1-button button">
-              <span className="desktop1-text">
-                <span>Camera 1</span>
-                <br></br>
-              </span>
-            </button>
-            <button type="button" className="desktop1-button1 button">
-              <span className="desktop1-text03">
-                <span>Camera 2</span>
-                <br></br>
-              </span>
-            </button>
-            <button type="button" className="desktop1-button2 button">
-              <span className="desktop1-text06">
-                <span>Camera 3</span>
-                <br></br>
-              </span>
-            </button>
-            <button type="button" className="desktop1-button3 button">
-              <span className="desktop1-text09">
-                <span>Camera 4</span>
-                <br></br>
-              </span>
-            </button>
-          </div>
-          <span className="desktop1-text12">
-            <span>Screenshot</span>
+    <div className="desktop1-rectangle1">
+      <div className="desktop1-frame1">
+        <button type="button" className="desktop1-button button">
+          <span className="desktop1-text">
+            <span>Camera 1</span>
+            <br></br>
           </span>
-        </div>
-
+        </button>
+        <button type="button" className="desktop1-button1 button">
+          <span className="desktop1-text03">
+            <span>Camera 2</span>
+            <br></br>
+          </span>
+        </button>
+        <button type="button" className="desktop1-button2 button">
+          <span className="desktop1-text06">
+            <span>Camera 3</span>
+            <br></br>
+          </span>
+        </button>
+        <button type="button" className="desktop1-button3 button">
+          <span className="desktop1-text09">
+            <span>Camera 4</span>
+            <br></br>
+          </span>
+        </button>
+      </div>
+      <span className="desktop1-text12">
+        <span>Screenshot</span>
+      </span>
+    </div>
   );
 };
 
@@ -81,40 +79,76 @@ function renderTasks(tasks: Task[], level = 0) {
     localStorage.setItem(task.name, JSON.stringify(updatedTask));
   };
 
-
-
   return tasks.map((task: Task) => {
     const Icon = () => {
-      if (!task.subTasks && tasks.length > 1) return <Checkbox
-        style={{ marginBottom: "10px", padding: "0px" }}
-        aria-label={task.name}
-        defaultChecked={task.checked}
-        color="success"
-        aria-checked={task.checked}
-        onChange={() => handleCheckboxChange(task)}
-      />;
-      else if (task.subTasks) return <SquareIcon style={{ marginBottom: "10px", padding: "0px" }}
-        scale={0.5}
-      />;
+      if (!task.subTasks && tasks.length > 1)
+        return (
+          <Checkbox
+            style={{ marginBottom: "10px", padding: "0px" }}
+            aria-label={task.name}
+            defaultChecked={task.checked}
+            color="success"
+            aria-checked={task.checked}
+            onChange={() => handleCheckboxChange(task)}
+          />
+        );
+      else if (task.subTasks)
+        return (
+          <SquareIcon
+            style={{ marginBottom: "10px", padding: "0px" }}
+            scale={0.5}
+          />
+        );
       else return <a></a>;
     };
 
     return (
-      <div style={{ paddingLeft: `${(level ** 0.5) * 30}px` }}>
+      <div style={{ paddingLeft: `${level ** 0.5 * 30}px` }}>
         <Col style={{ minHeight: "45px" }}>
-          <div style={{ display: "flex", flex: 1, minHeight: "2px", alignItems: "flex-start" }}>
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              minHeight: "2px",
+              alignItems: "flex-start",
+            }}
+          >
             <Icon />
-            {(!task.subTasks) && <div style={{ marginLeft: "10px", flex: 1, wordBreak: "break-word", alignItems: "flex-start" }}>{task.name} ({task.points })</div>}
-            {(task.subTasks) && <div style={{ marginLeft: "10px", flex: 1, wordBreak: "break-word", alignItems: "flex-start", fontWeight: "bolder", paddingBottom: "10px" }}>{task.name} :</div>}
-        </div>
-        {task.subTasks && renderTasks(task.subTasks, level + 1)}
-      </Col>
-    </div>
+            {!task.subTasks && (
+              <div
+                style={{
+                  marginLeft: "10px",
+                  flex: 1,
+                  wordBreak: "break-word",
+                  alignItems: "flex-start",
+                }}
+              >
+                {task.name} ({task.points})
+              </div>
+            )}
+            {task.subTasks && (
+              <div
+                style={{
+                  marginLeft: "10px",
+                  flex: 1,
+                  wordBreak: "break-word",
+                  alignItems: "flex-start",
+                  fontWeight: "bolder",
+                  paddingBottom: "10px",
+                }}
+              >
+                {task.name} :
+              </div>
+            )}
+          </div>
+          {task.subTasks && renderTasks(task.subTasks, level + 1)}
+        </Col>
+      </div>
     );
   });
 }
 
-function SubList(props: { name: string, tasks: Task[] }) {
+function SubList(props: { name: string; tasks: Task[] }) {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [score, setScore] = useState<number>(0);
   const [totalScore, setTotalScore] = useState<number>(0);
@@ -131,7 +165,6 @@ function SubList(props: { name: string, tasks: Task[] }) {
     // const calculateScore = (tasks: Task[]): number => {
     //   return tasks.reduce((total, task) => total + (task.checked ? task.points : 0) + (task.subTasks ? calculateScore(task.subTasks) : 0), 0);
     // };
-
 
     const calculateTotalScore = (tasks: Task[]): number => {
       let total = 0;
@@ -150,19 +183,23 @@ function SubList(props: { name: string, tasks: Task[] }) {
     setTotalScore(calculateTotalScore(tasks));
   }, [tasks]);
 
-
   return (
-    <div style={{ paddingLeft: "20px", paddingRight: "20px", paddingBottom: "10px" }}>
+    <div
+      style={{
+        paddingLeft: "20px",
+        paddingRight: "20px",
+        paddingBottom: "10px",
+      }}
+    >
       <Row>
-        <h2>{props.name} ({score}/{totalScore})</h2>
-        <div>
-          {renderTasks(tasks)}
-        </div>
+        <h2>
+          {props.name} ({score}/{totalScore})
+        </h2>
+        <div>{renderTasks(tasks)}</div>
       </Row>
     </div>
   );
 }
-
 
 function SideBar() {
   const [collapsed, setCollapsed] = useState(false);
@@ -172,12 +209,12 @@ function SideBar() {
   };
   const styles = {
     sideBarHeight: {
-      height: "145vh"
+      height: "145vh",
     },
     menuIcon: {
       float: "right",
-      margin: "10px"
-    }
+      margin: "10px",
+    },
   };
 
   return (
@@ -204,17 +241,32 @@ function SideBar() {
         Tasks
         <div style={{ height: "5px" }} />
         <MenuSquareIcon />
-      </IconButton >
-      <Sidebar style={styles.sideBarHeight} collapsed={collapsed} rtl={false} width="700px" collapsedWidth="0px" backgroundColor="rgb(0, 0, 69, 0.7)">
-      <Menu>
+      </IconButton>
+      <Sidebar
+        style={styles.sideBarHeight}
+        collapsed={collapsed}
+        rtl={false}
+        width="700px"
+        collapsedWidth="0px"
+        backgroundColor="rgb(0, 0, 69, 0.7)"
+      >
+        <Menu>
           <MenuItem icon={<Home />}>Home</MenuItem>
-          <SubList name="TASK 1 : Coastal Pioneer Array" tasks={taskJSON.task1.tasks} />
-          <SubList name="TASK 2 : Deploy SMART cables" tasks={taskJSON.task2.tasks} />
-          <SubList name="TASK 3 : From the Red Sea to Tenesse" tasks={taskJSON.task3.tasks} />
+          <SubList
+            name="TASK 1 : Coastal Pioneer Array"
+            tasks={taskJSON.task1.tasks}
+          />
+          <SubList
+            name="TASK 2 : Deploy SMART cables"
+            tasks={taskJSON.task2.tasks}
+          />
+          <SubList
+            name="TASK 3 : From the Red Sea to Tenesse"
+            tasks={taskJSON.task3.tasks}
+          />
           <SubList name="TASK 4 : MATE Floats" tasks={taskJSON.task4.tasks} />
           {/* More menu items... */}
-          
-      </Menu>
+        </Menu>
       </Sidebar>
     </div>
   );
@@ -228,39 +280,27 @@ function CSVHandler() {
     LineElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    BarElement
   );
   const [chartData, setChartData] = useState<ChartData>({
-  labels: [],
-  datasets: [],
-});
+    labels: [],
+    datasets: [],
+  });
   const [fileSelected, setFileSelected] = useState(false);
-  
-   const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart',
-    },
-  },
-   };
-//   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-// const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: 'Dataset 1',
-//       data: [234,4,345,45,4,45,45,5],
-//       borderColor: 'rgb(255, 99, 132)',
-//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//     }
-//   ],
-// };
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Number of Sturgeon detected at each receiver over time",
+      },
+    },
+  };
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files) return;
@@ -274,7 +314,7 @@ function CSVHandler() {
         const lines = contents.split("\n");
         const header = lines[0].split(",");
 
-        const datasets: ChartData['datasets'] = [];
+        const datasets: ChartData["datasets"] = [];
         const dataLists: { [key: string]: number }[] = [];
         const labels: string[] = [];
 
@@ -292,53 +332,27 @@ function CSVHandler() {
         }
 
         const colors = [
-  'rgb(255, 99, 132)',
-  'rgb(75, 192, 192)',
-  'rgb(255, 205, 86)',
+          "rgb(0, 143, 136)",
+          "rgb(245, 200, 0)",
+          "rgb(241, 98, 8)",
         ];
-        
-        Object.keys(dataLists[0]).forEach((key,index) => {
+
+        Object.keys(dataLists[0]).forEach((key, index) => {
           const color = colors[index % colors.length];
           datasets.push({
             label: key,
             data: dataLists.map((item) => item[key]),
-           borderColor: color,
-    backgroundColor: `${color.slice(0, -1)}, 0.3)`,
-            
+            borderColor: `${color.slice(0, -1)}, 0.75)`,
+            backgroundColor: `${color.slice(0, -1)}, 0.95)`,
           });
         });
         console.log("Parsed Data:", dataLists);
         console.log("Data sets:", datasets);
 
-    //     for (let i = 1; i < lines.length; i++) {
-    //       const tempData = []
-    //       const line = lines[i].split(",");
-    //       const item: { [key: string]: number } = {};
-    //       labels.push(line[0]?.trim() ?? "label ??");
-
-    //       for (let j = 0; j < header.length; j++) {
-    //         const key = header[j]?.trim() ?? "key ??";
-    //         const value = line[j]?.trim() ?? "700";
-    //         item[key] = Number(value);
-    //         tempData.push(Number(value));
-    //       }
-
-    //       datasets.push({
-    //   label: 'Dataset 1',
-    //   data: tempData,
-    //   borderColor: 'rgb(255, 99, 132)',
-    //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    // });
-    //     }
-
-    //     console.log("Parsed Data:", parsedData);
-
-        const data:ChartData = {
-          labels,//parsedData.map(item => item.x), // Replace 'x' with the actual property name for the x-axis
+        const data: ChartData = {
+          labels, //parsedData.map(item => item.x), // Replace 'x' with the actual property name for the x-axis
           datasets,
-          
         };
-       
 
         setChartData(data);
       }
@@ -350,12 +364,38 @@ function CSVHandler() {
     }
   };
 
-  return (
-    <div>
-      <input type="file" accept=".csv" onChange={handleFileChange} />
+return (
+  <div>
+    <input type="file" accept=".csv" onChange={handleFileChange} />
+    <div
+      style={{
+        backgroundColor: "rgba(225, 225, 225, 0.8)",
+        margin: "3vh",
+        padding: "10px",
+        borderRadius: "10px",
+        backdropFilter: "blur(10px)",
+        WebkitBackdropFilter: "blur(10px)",
+        border: "1px solid rgba(255, 255, 255, 0.18)",
+        // height: chartData.datasets.length > 0 ? undefined : "150px",
+      }}
+    >
       <Line data={chartData as any} options={options} />
     </div>
-  );
+    <div
+      style={{
+        backgroundColor: "rgba(225, 225, 225, 0.8)",
+        margin: "3vh",
+        padding: "10px",
+        borderRadius: "10px",
+        backdropFilter: "blur(10px)",
+        WebkitBackdropFilter: "blur(10px)",
+        border: "1px solid rgba(255, 255, 255, 0.18)",
+      }}
+    >
+      <Bar data={chartData as any} options={options} />
+    </div>
+  </div>
+);
 }
 
 export function ControllerApp() {
@@ -404,35 +444,38 @@ export function ControllerApp() {
     }, "image/png");
   };
 
-  
-
-    const streamUrl = "http://localhost:8880/";
+  const streamUrl = "http://localhost:8880/";
   const styles = {
     contentDiv: {
-      display: "flex"
+      display: "flex",
     },
     contentMargin: {
       marginLeft: "10px",
-      width: "100%"
+      width: "100%",
     },
-    
   };
-  
+
   return (
     <>
-      <div style={{ ...styles.contentDiv, position: "fixed", right: 0, zIndex: 1000 }}>
+      <div
+        style={{
+          ...styles.contentDiv,
+          position: "fixed",
+          right: 0,
+          zIndex: 1000,
+        }}
+      >
         <SideBar />
       </div>
 
-      <Row>
+      <Row className="justify-content-center">
         <Col lg={3}>
           <div style={styles.contentDiv}>
             <ScreenshotVeiw />
           </div>
-      <CSVHandler/>
+          <CSVHandler />
         </Col>
       </Row>
     </>
   );
-
 }
