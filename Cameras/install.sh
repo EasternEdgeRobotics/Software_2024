197543224a2e437f62166298a31d03f8c7abd93c
Evan <41494790+evaan@users.noreply.github.com>
2024-04-27 12:11:47 -0230

    Make bash command work regardless of user and path

--- Cameras/install.sh
+++ Cameras/install.sh
@@ -2,12 +2,23 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y python3-picamera2 python3-libcamera git make
 git clone https://github.com/roamingthings/spyglass
+sudo cp spyglass.conf ~/printer_data/config/spyglass.conf
 cd spyglass
 mkdir -p ~/printer_data/config
-make install
-cd ..
-sudo cp spyglass.conf ~/printer_data/config/spyglass.conf
-sudo cp spyglass.service /etc/systemd/system/spyglass.service
+echo "
+[Unit]
+Description=Spyglass Camera Streamer
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+User=$USER
+ExecStart=python3 $PWD/run.py -f 60 -r "1280x720"
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/spyglass.service
 sudo systemd daemon-reload
 sudo systemctl enable --now spyglass
 echo "all done, make sure to reboot"
