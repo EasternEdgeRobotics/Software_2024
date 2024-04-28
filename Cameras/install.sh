5987014abf75a9b38cad4b13c8986baa7f863f09
Zaid-Duraid <zaduraid@mun.ca>
2024-04-28 11:49:17 -0230

    fix: Fixed minor issue with command order in Cameras/install.sh

--- Cameras/install.sh
+++ Cameras/install.sh
@@ -2,10 +2,18 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y python3-picamera2 python3-libcamera git make
+
+# Clone and make git repo
 git clone https://github.com/roamingthings/spyglass
-sudo cp spyglass.conf ~/printer_data/config/spyglass.conf
 cd spyglass
+make install
+cd ..
+
+# Copy the spyglass configuration such that it is seen by the spyglass application
 mkdir -p ~/printer_data/config
+sudo cp spyglass.conf ~/printer_data/config/spyglass.conf
+
+# Create the service
 echo "
 [Unit]
 Description=Spyglass Camera Streamer
@@ -22,4 +30,5 @@ WantedBy=multi-user.target
 " | sudo tee /etc/systemd/system/spyglass.service
 sudo systemd daemon-reload
 sudo systemctl enable --now spyglass
+
 echo "all done, make sure to reboot"
